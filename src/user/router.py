b5b056378701ab939a56a83f5b94f42812348bf2
Gratix0 <oganisyan.sanya1@mail.ru>
2024-10-30 00:47:58 +0300

    async watermark avatar

--- src/user/router.py
+++ src/user/router.py
@@ -1,25 +1,65 @@
-from sys import base_prefix
 
-from fastapi import APIRouter, Depends
+from pathlib import Path
+
+from typing import Optional
+
+
+from fastapi import APIRouter, Depends, UploadFile, HTTPException, File
+from pydantic import EmailStr
 from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
 
 from epg_test_task.src.database import get_db
 from epg_test_task.src.user.dependencies import check_unique_email
-from epg_test_task.src.user.schemas import UserCreate
-from epg_test_task.src.user.service import create_user
+from epg_test_task.src.user.schemas import UserInApi, AvatarUser, UserCreate
+from epg_test_task.src.user.service import create_user, allowed_file, watermark_my_image
 
-router = APIRouter(base_prefix="clients/")
+router = APIRouter(prefix="/api/clients")
 
 @router.post("/create", summary="Register a new user", description="Creates a new user account with the provided data.")
-async def add_user(user: UserCreate, db: Session = Depends(get_db), _ = Depends(check_unique_email)):
+async def add_user(first_name: str,
+                   last_name: str,
+                   email: EmailStr,
+                   password: str,
+                   gender: str,
+                   db: Session = Depends(get_db),
+                   _ = Depends(check_unique_email),
+                   avatar: UploadFile = File(None)):
     """
     Registers a new user.
 
-    - **user**: Data for creating a new user account.
+    - **first_name**: First name of the user.
+    - **last_name**: Last name of the user.
+    - **email**: Email address of the user.
+    - **password**: Password for the user account.
+    - **gender**: Gender of the user.
     - **db**: Database session, extracted from the dependency.
     - **_**: Dependency to check if the username is unique.
 
     **Returns**:
     - The created user object.
     """
-    return await create_user(user, db)
+    user_data = UserInApi(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        password=password,
+        gender=gender
+    )
+
+    local_avatar_path = "None"
+    if avatar:
+        local_avatar_path = await watermark_my_image(user_data.email, avatar)
+
+    user_data = UserCreate(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        password=password,
+        gender=gender,
+        pic_url=local_avatar_path
+    )
+
+    new_user = await create_user(user_data, db)
+    return new_user
+
